23f7beaa2d4af4f7a18f893b8257cc31397d2f02
Saidakbar Gaziev <si.gaziev@gmail.com>
2023-05-24 03:56:49 +0300

    removed solana code from repository

--- containers/docker/base/solana/programs/bpf/rust/panic/src/lib.rs
+++
@@ -1,8 +0,0 @@
-//! @brief Example Rust-based BPF program that panics
-
-extern crate solana_program;
-
-#[no_mangle]
-pub extern "C" fn entrypoint(_input: *mut u8) -> u64 {
-    panic!();
-}
--- containers/docker/base/solana/tokens/src/lib.rs
+++
@@ -1,4 +0,0 @@
-pub mod arg_parser;
-pub mod args;
-pub mod commands;
-mod db;
